68cdaadfa731500a022d22d0f7b7c2ed1bbdaf67
EGYx0 <a70609270@gmail.com>
2026-08-04 10:08:20 +0300

    refactor(folder): organize exercises into one folder

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,28 +3,3 @@ export interface IPerson {
   age: number;
   isMarried: boolean;
 }
-
-const userName = "Ahmed";
-const age = 24;
-const isStudent = false;
-
-const skills: string[] = ["Coding", "Gym", "Watching movies"];
-const scores: number[] = [1, 2, 3, 4, 5];
-
-const theme: "light" | "dark" = "dark";
-
-const user = {
-  userName,
-  age,
-  isStudent,
-};
-
-function printUser(user: {
-  userName: string;
-  age: number;
-  isStudent: boolean;
-}): void {
-  console.log(`${user.userName} is ${user.age} years old.`);
-}
-
-printUser(user);
